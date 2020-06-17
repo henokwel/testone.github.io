08cab82c875c969570418853b57eebf9d7916be4
henWel <henokwel@outlook.com>
2020-06-18 01:38:36 +0200

    small fn

--- js.js
+++ js.js
@@ -1,7 +1,30 @@
 let card = document.querySelector('.card');
+//Button
+let aboutMeBtn = document.querySelector('.aboutMe')
+let backToProfile = document.querySelector('.backToProfile')
+// Div's
+let cardContent = document.querySelector('.card-content')
+let showCard = document.querySelector('.card-back-content')
 
 document.addEventListener('mousemove', function (e) {
-    let xAxis = (window.innerWidth / 1 - e.pageX) / 1000;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 1000;
+    let xAxis = (window.innerWidth / 1 - e.pageX) / 300;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 950;
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
+
+// document.addEventListener('click', (e) => {
+//     console.log('Show Me');
+//     cardContent.className="active"
+//     showCard.className="inActive"
+
+// })
+
+aboutMeBtn.addEventListener('click', () => {
+    showCard.className = "inActive"
+    cardContent.className = "active"
+})
+
+backToProfile.addEventListener('click', () => {
+    showCard.className = "card-back-content"
+    cardContent.className = "card-content"
+})
